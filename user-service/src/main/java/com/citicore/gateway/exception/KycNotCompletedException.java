37a0969731d024d1dfac5dc2be08b5a9862d74c3
rabbaniinamdar <mahammadrabbani822931@gmail.com>
2026-09-05 12:30:54 +0530

    test: multi commit account

--- user-service/src/main/java/com/citicore/gateway/exception/KycNotCompletedException.java
+++
@@ -1,8 +0,0 @@
-package com.citicore.gateway.exception;
-
-public class KycNotCompletedException extends RuntimeException {
-
-    public KycNotCompletedException(String message) {
-        super(message);
-    }
-}
